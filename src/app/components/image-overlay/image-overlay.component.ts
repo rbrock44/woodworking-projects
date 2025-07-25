3f7615c2e1f47b090a024a3366e8584d3f5c5898
rbrock44 <rbrock444@gmail.com>
2025-07-25 18:01:51 -0500

    add overlay side buttons

--- src/app/components/image-overlay/image-overlay.component.ts
+++ src/app/components/image-overlay/image-overlay.component.ts
@@ -13,6 +13,8 @@ import {CommonModule} from '@angular/common';
 export class ImageOverlayComponent implements OnInit {
   @Input() imageSrc: string = '';
   @Output() close = new EventEmitter<void>();
+  @Output() previous = new EventEmitter<void>();
+  @Output() next = new EventEmitter<void>();
 
   imageWidth = 0;
   imageHeight = 0;
